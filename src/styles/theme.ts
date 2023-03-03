d6095a8bfcd2aa4936dbf536107aa40f487b9fd8
Lucas Melo <contatolucasgmelo@gmail.com>
2023-03-03 17:04:20 -0300

    chore: config theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,38 @@
+export const theme = {
+  colors: {
+    bg: '#302C42',
+    secondary: '#A093DB',
+    bgLight: '#817C99',
+    white: '#fff',
+    grey: '#E3E3E3',
+    black: '#484C4F'
+  },
+  fonts: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem'
+    },
+    spacings: {
+      xxsmall: '0.8rem',
+      xsmall: '1.6rem',
+      small: '2.4rem',
+      medium: '3.2rem',
+      large: '4.0rem',
+      xlarge: '4.8rem',
+      xxlarge: '5.6rem'
+    },
+    layers: {
+      base: 10,
+      menu: 20,
+      overlay: 30,
+      modal: 40,
+      alwaysOnTop: 50
+    }
+  }
+}
